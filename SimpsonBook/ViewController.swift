7f182337177f484e5395f503ddd83935cd4ced6c
Ali Rıza <aliriza.ihn@gmail.com>
2022-10-18 16:34:02 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Ali Rıza İLHAN on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
